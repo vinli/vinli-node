e46d54e53bcf4b507c1d16298bd722d66f236be0
Zac Barnes <zac.barnes89@gmail.com>
2016-03-09 14:26:14 -0600

    incorporate distance service into SDK

--- test/vehicle_tests.js
+++ test/vehicle_tests.js
@@ -371,4 +371,273 @@ describe('Vehicle', function() {
         });
     });
   });
+
+  describe('#odometers()', function() {
+    it('should exist', function() {
+      var vehicle = Vinli.Vehicle.forge('asfdafdasfdsdf');
+      expect(vehicle).to.have.property('odometers').that.is.a('function');
+    });
+
+    it('should return a list of odometers for the vehicle', function() {
+      var m = nock('https://distance.vin.li')
+        .get('/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f/odometers?limit=2')
+        .reply(200, {
+          odometers: [
+            {
+              id: 'bcdc8734-ce79-4d78-a911-f77c09316f5f',
+              vehicleId: '0e14f2db-ff0b-43bd-b88c-01b9f226778f',
+              reading: 83321969.16,
+              date: '2016-03-03T20:23:53.726Z',
+              links: {
+                vehicle: 'https://platform-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f'
+              }
+            },
+            {
+              id: '428eb67d-bcc5-4870-8bab-96abb30aaaf2',
+              vehicleId: '0e14f2db-ff0b-43bd-b88c-01b9f226778f',
+              reading: 83307485.1,
+              date: '2016-03-02T16:33:48.780Z',
+              links: {
+                vehicle: 'https://platform-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f'
+              }
+            }
+          ],
+          meta: {
+            pagination: {
+              remaining: 2,
+              until: '2016-03-09T18:07:16.150Z',
+              since: '1970-01-01T00:00:00.000Z',
+              limit: 2,
+              sortDir: 'desc',
+              links: {
+                prior: 'https://distance-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f/odometers?limit=2&until=1456936428779'
+              }
+            }
+          }
+        });
+
+      return Vinli.Vehicle.forge('0e14f2db-ff0b-43bd-b88c-01b9f226778f').odometers({ limit: 2 })
+        .then(function(odometers) {
+          expect(odometers).to.have.property('list').that.is.an('array');
+          expect(odometers.list).to.have.lengthOf(2);
+          expect(odometers.list[0]).to.be.instanceOf(Vinli.Odometer);
+          expect(odometers).to.have.property('prior').that.is.a('function');
+
+          m.done();
+        });
+    });
+  });
+
+  describe('#setOdometer()', function() {
+    it('should exist', function() {
+      var vehicle = Vinli.Vehicle.forge('asfdafdasfdsdf');
+      expect(vehicle).to.have.property('setOdometer').that.is.a('function');
+    });
+
+    it('should return an Odometer object', function() {
+      var odo = {
+        reading: 5000,
+        unit: 'km'
+      };
+
+      var m = nock('https://distance.vin.li')
+        .post('/api/v1/vehicles/78809613-7d24-40d6-a76b-a4bc8af3a181/odometers', {
+          odometer: odo
+        }).reply(201, {
+          odometer: {
+            id: 'f5726b65-cc57-4f19-aec5-8abd77bbb814',
+            vehicleId: '78809613-7d24-40d6-a76b-a4bc8af3a181',
+            reading: 5000,
+            date: '2015-12-02T16:02:37.315Z',
+            links: {
+              vehicle: 'https://platform-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f'
+            }
+          }
+        });
+
+      return Vinli.Vehicle.forge('78809613-7d24-40d6-a76b-a4bc8af3a181').setOdometer(odo)
+        .then(function(odometer) {
+          expect(odometer).to.be.an.instanceOf(Vinli.Odometer);
+          expect(odometer).to.have.property('id').that.is.a('string');
+          expect(odometer).to.have.property('vehicleId', '78809613-7d24-40d6-a76b-a4bc8af3a181');
+          expect(odometer).to.have.property('reading', 5000);
+          expect(odometer).to.have.property('date').that.is.a('string');
+          expect(odometer).to.have.property('links').that.is.an('object');
+
+          m.done();
+        });
+    });
+  });
+
+  describe('#odometerTriggers()', function() {
+    it('should exist', function() {
+      var vehicle = Vinli.Vehicle.forge('asfdafdasfdsdf');
+      expect(vehicle).to.have.property('odometerTriggers').that.is.a('function');
+    });
+
+    it('should return a list of odometerTriggers for the vehicle', function() {
+      var m = nock('https://distance.vin.li')
+        .get('/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f/odometer_triggers')
+        .reply(200, {
+          odometerTriggers: [
+            {
+              id: 'bcdc8734-ce79-4d78-a911-f77c09316f5f',
+              vehicleId: '0e14f2db-ff0b-43bd-b88c-01b9f226778f',
+              type: 'specific',
+              threshold: 83321969.16,
+              events: 0,
+              links: {
+                vehicle: 'https://platform-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f'
+              }
+            },
+            {
+              id: '428eb67d-bcc5-4870-8bab-96abb30aaaf2',
+              vehicleId: '0e14f2db-ff0b-43bd-b88c-01b9f226778f',
+              type: 'from_now',
+              threshold: 5000,
+              events: 0,
+              links: {
+                vehicle: 'https://platform-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f'
+              }
+            }
+          ],
+          meta: {
+            pagination: {
+              remaining: 2,
+              until: '2016-03-09T18:07:16.150Z',
+              since: '1970-01-01T00:00:00.000Z',
+              limit: 2,
+              sortDir: 'desc',
+              links: {
+                prior: 'https://distance-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f/odometer_triggers?&until=1456936428779'
+              }
+            }
+          }
+        });
+
+      return Vinli.Vehicle.forge('0e14f2db-ff0b-43bd-b88c-01b9f226778f').odometerTriggers()
+        .then(function(triggers) {
+          expect(triggers).to.have.property('list').that.is.an('array');
+          expect(triggers.list).to.have.lengthOf(2);
+          expect(triggers.list[0]).to.be.instanceOf(Vinli.OdometerTrigger);
+
+          m.done();
+        });
+    });
+  });
+
+  describe('#setOdometerTrigger()', function() {
+    it('should exist', function() {
+      var vehicle = Vinli.Vehicle.forge('asfdafdasfdsdf');
+      expect(vehicle).to.have.property('setOdometerTrigger').that.is.a('function');
+    });
+
+    it('should return an OdometerTrigger object', function() {
+      var trig = {
+        threshold: 5000,
+        type: 'from_now',
+        unit: 'km'
+      };
+
+      var m = nock('https://distance.vin.li')
+        .post('/api/v1/vehicles/78809613-7d24-40d6-a76b-a4bc8af3a181/odometer_triggers', {
+          odometerTrigger: trig
+        }).reply(201, {
+          odometerTrigger: {
+            id: 'f5726b65-cc57-4f19-aec5-8abd77bbb814',
+            vehicleId: '78809613-7d24-40d6-a76b-a4bc8af3a181',
+            threshold: 5000,
+            type: 'from_now',
+            events: 0,
+            links: {
+              vehicle: 'https://platform-dev.vin.li/api/v1/vehicles/0e14f2db-ff0b-43bd-b88c-01b9f226778f'
+            }
+          }
+        });
+
+      return Vinli.Vehicle.forge('78809613-7d24-40d6-a76b-a4bc8af3a181').setOdometerTrigger(trig)
+        .then(function(trigger) {
+          expect(trigger).to.be.an.instanceOf(Vinli.OdometerTrigger);
+          expect(trigger).to.have.property('id').that.is.a('string');
+          expect(trigger).to.have.property('vehicleId', '78809613-7d24-40d6-a76b-a4bc8af3a181');
+          expect(trigger).to.have.property('threshold', 5000);
+          expect(trigger).to.have.property('type', 'from_now');
+          expect(trigger).to.have.property('events', 0);
+          expect(trigger).to.have.property('links').that.is.an('object');
+
+          m.done();
+        });
+    });
+  });
+
+  describe('#distances()', function() {
+    it('should exist', function() {
+      var vehicle = Vinli.Vehicle.forge('asfdafdasfdsdf');
+      expect(vehicle).to.have.property('distances').that.is.a('function');
+    });
+
+    it('should return an array of distances', function() {
+      var m = nock('https://distance.vin.li', { reqHeaders: { 'x-vinli-unit': 'mi' } })
+        .get('/api/v1/vehicles/78809613-7d24-40d6-a76b-a4bc8af3a181/distances')
+        .reply(200, {
+          distances: [
+            {
+              confidenceMin: 3628.139,
+              confidenceMax: 100032.986,
+              value: 51830.563,
+              lastOdometerDate: '2016-03-03T20:23:53.726Z'
+            },
+            {
+              confidenceMin: 51831.166,
+              confidenceMax: 51832.409,
+              value: 51831.788,
+              lastOdometerDate: '2016-03-03T20:23:53.726Z'
+            }
+          ]
+        });
+
+      return Vinli.Vehicle.forge('78809613-7d24-40d6-a76b-a4bc8af3a181').distances({ unit: 'mi' })
+        .then(function(distances) {
+          expect(distances).to.be.an('array').that.has.length(2);
+          expect(distances[0]).to.be.an('object');
+          expect(distances[0]).to.have.property('confidenceMin');
+          expect(distances[0]).to.have.property('confidenceMax');
+          expect(distances[0]).to.have.property('value');
+          expect(distances[0]).to.have.property('lastOdometerDate');
+
+          m.done();
+        });
+    });
+  });
+
+  describe('#bestDistance()', function() {
+    it('should exist', function() {
+      var vehicle = Vinli.Vehicle.forge('asfdafdasfdsdf');
+      expect(vehicle).to.have.property('bestDistance').that.is.a('function');
+    });
+
+    it('should return a distance', function() {
+      var m = nock('https://distance.vin.li', { reqHeaders: { 'x-vinli-unit': 'mi' } })
+        .get('/api/v1/vehicles/78809613-7d24-40d6-a76b-a4bc8af3a181/distances/_best')
+        .reply(200, {
+          distance: {
+              confidenceMin: 51831.166,
+              confidenceMax: 51832.409,
+              value: 51831.788,
+              lastOdometerDate: '2016-03-03T20:23:53.726Z'
+            }
+        });
+
+      return Vinli.Vehicle.forge('78809613-7d24-40d6-a76b-a4bc8af3a181').bestDistance({ unit: 'mi' })
+        .then(function(distance) {
+          expect(distance).to.be.an('object');
+          expect(distance).to.have.property('confidenceMin');
+          expect(distance).to.have.property('confidenceMax');
+          expect(distance).to.have.property('value');
+          expect(distance).to.have.property('lastOdometerDate');
+
+          m.done();
+        });
+    });
+  });
 });
